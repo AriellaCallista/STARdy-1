b77ad49374651711e93b4418ee541a1a6ba90c98
Stella Lye <e0967875@u.nus.edu>
2023-05-28 14:47:15 +0800

    Added SignUp Page

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { useFonts } from 'expo-font';
 
 import Welcome from './src/screens/welcome';
 import SignUp from './src/screens/signup';
+import MainTab from './src/navigation/mainTab';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -47,6 +48,7 @@ export default function App() {
         })}>
           <Stack.Screen name='Welcome' component={Welcome} options={{ headerShown: false }}/>
           <Stack.Screen name='Sign Up' component={SignUp} options={{ headerShown: false }}/>
+          <Stack.Screen name='MainTab' children={MainTab} options={{ headerShown: false }}/>
         </Stack.Navigator>
       </NavigationContainer>
     )

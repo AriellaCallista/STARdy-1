6da9541fd98354de21d8aab5dacb456708ca75bf
Stella Lye <e0967875@u.nus.edu>
2023-05-28 15:55:44 +0800

    Added SessionUsers page frontend

--- src/screens/sessionUsers.js
+++ src/screens/sessionUsers.js
@@ -1,12 +1,50 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { Component, useState } from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  Alert,
+  TextInput,
+  FlatList,
+} from 'react-native'
+import { FontAwesome } from '@expo/vector-icons'; 
+import { SimpleLineIcons } from '@expo/vector-icons';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'; 
 
-const SessionUsers = () => {
-  return (
-    <View>
-      <Text>SessionUsers</Text>
-    </View>
-  )
-}
+import Friends from '../components/sessionUsers/friends';
+import Random from '../components/sessionUsers/random'; 
+import Add from '../components/sessionUsers/add';
 
-export default SessionUsers;
+
+export default function SessionUsers() {
+
+  const focusSession = createMaterialTopTabNavigator();
+
+  return <focusSession.Navigator 
+            screenOptions={{
+              tabBarLabelStyle: { fontSize: 14 },
+              tabBarLabelStyle: { color: '#f6f6f6'},
+              tabBarStyle: { backgroundColor: '#007788', paddingTop: 50 }, 
+              
+            }}
+          >
+
+    <focusSession.Screen 
+      name='Friends' 
+      component={Friends} 
+    />
+
+    <focusSession.Screen 
+      name='Random' 
+      component={Random}
+      />
+
+    <focusSession.Screen 
+      name='Add' 
+      component={Add}
+      />
+
+  </focusSession.Navigator>
+} 

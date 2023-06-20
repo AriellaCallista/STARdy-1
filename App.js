7dd499b222a9f9a43af4b9eda8cde509ccadae9f
STELLA-LYE <e0967875@u.nus.edu>
2023-06-20 15:16:38 +0800

    updated main

--- App.js
+++ App.js
@@ -1,89 +1,12 @@
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
-import { useFonts } from 'expo-font';
-
-import Welcome from './src/screens/welcome';
-import SignUp from './src/screens/signup';
-import MainTab from './src/navigation/mainTab';
-
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import { Feather } from '@expo/vector-icons'
-
-const Stack = createNativeStackNavigator();
+import { View, Text } from 'react-native'
+import React from 'react'
 
 export default function App() {
 
-
-  const[fontsLoaded] = useFonts({
-    PressStart: require('./assets/fonts/PressStart2P-Regular.ttf'),
-    RowdiesBold: require('./assets/fonts/Rowdies-Bold.ttf'),
-    RowdiesRegular: require('./assets/fonts/Rowdies-Regular.ttf'),
-    UbuntuBold: require('./assets/fonts/Ubuntu-Bold.ttf'),
-    UbuntuMedium: require('./assets/fonts/Ubuntu-Medium.ttf')
-  });
-
-
-  if (!fontsLoaded) {
-    return null;
-  } else {
     return (
-
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={({ navigation }) => ({
-          headerTitleStyle: {
-            color: '#f6f6f6'
-          },
-          headerStyle: {
-            backgroundColor: '#007788'
-          },
-          headerTitleAlign: 'center',
-          headerLeft: () => (
-            <Feather
-              name='arrow-left' 
-              size={22} 
-              onPress={() => navigation.goBack() }
-              color='#f6f6f6' />
-          )
-        })}>
-          <Stack.Screen name='Welcome' component={Welcome} options={{ headerShown: false }}/>
-          <Stack.Screen name='Sign Up' component={SignUp} options={{ headerShown: false }}/>
-          <Stack.Screen name='MainTab' children={MainTab} options={{ headerShown: false }}/>
-        </Stack.Navigator>
-      </NavigationContainer>
+      <View>
+        <Text> Main </Text>
+      </View>
     )
   }
-}
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#eef1e1',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-//   mainTab: {
-//     position: 'absolute',
-//     backgroundColor: '#eef1e1',
-//   },
-//   icon: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   }, 
-//   shadow: {
-//     shadowColor: '#007788',
-//     shadowOffset: {
-//       width: 0,
-//       height: 10,
-//     },
-//     shadowOpacity: 1,
-//     shadowRadius: 3.5,
-//     elevation: 5,
-//     zIndex: 999 
-//   },
-//   iconContainer: {
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   }
-// });
+  
--- src/screens/welcome.js
+++ src/screens/welcome.js
@@ -1,58 +1,12 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, Text, TouchableOpacity, Alert, Image } from 'react-native';
+import { View, Text } from 'react-native'
+import React from 'react'
 
-
-
-export default function Welcome({ navigation }) {
-
-    const pressHandler = () => {
-        navigation.navigate('Sign Up');
-    }
-
-    return (
-        <View style={styles.container}>
-            <Text style={styles.titleText}>STARdy</Text>
-                <TouchableOpacity style={styles.button} onPress={pressHandler} activeOpacity={0.5}>
-                    <Text style={styles.buttonText}>Start Today</Text>
-                </TouchableOpacity>
-            <Image source={require('../../assets/star-icon.png')} style={styles.image} />
-        </View>
-    );
+const Welcome = () => {
+  return (
+    <View>
+      <Text>Welcome</Text>
+    </View>
+  )
 }
 
-const styles=StyleSheet.create({
-    container: {
-        flex:1,
-        backgroundColor: '#eef1e1',
-        alignItems: 'center',
-    },
-    titleText: {
-        color: '#007788',
-        fontFamily: 'PressStart',
-        fontSize: 52,
-        position: 'absolute',
-        top: '25%'        
-    }, 
-    button: {
-        padding: 19,
-        backgroundColor: '#007788',
-        position: 'absolute',
-        bottom: '25%',
-        borderRadius: 15
-    },
-    buttonText: {
-        fontSize: 32,
-        fontFamily: 'RowdiesRegular',
-        color: '#f6f6f6'
-    }, 
-    image: {
-        width: 250,
-        height: 250,
-        position: 'absolute',
-        top: '34%'
-    }
-})
-    
-
-
-
+export default Welcome

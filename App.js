4d3b53ef60a9a4c40e720dac26638b81ad42e831
STELLA-LYE <e0967875@u.nus.edu>
2023-06-20 15:33:47 +0800

    Update welcome with notifiction

--- App.js
+++ App.js
@@ -1,12 +1,62 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Alert, StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+
+import Welcome from './src/screens/welcome';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 
 export default function App() {
 
+  const Stack = createNativeStackNavigator();
+
+  const[fontsLoaded] = useFonts({
+    PressStart: require('./assets/fonts/PressStart2P-Regular.ttf'),
+    RowdiesBold: require('./assets/fonts/Rowdies-Bold.ttf'),
+    RowdiesRegular: require('./assets/fonts/Rowdies-Regular.ttf'),
+    UbuntuBold: require('./assets/fonts/Ubuntu-Bold.ttf'),
+    UbuntuMedium: require('./assets/fonts/Ubuntu-Medium.ttf')
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  } else {
     return (
-      <View>
-        <Text> Main </Text>
-      </View>
+
+      <NavigationContainer>
+        <NoteProvider>
+          <Stack.Navigator screenOptions={({ navigation }) => ({
+            headerTitleStyle: {
+              color: '#f6f6f6'
+            },
+            headerStyle: {
+              backgroundColor: '#007788'
+            },
+            headerTitleAlign: 'center',
+            headerLeft: () => (
+              <Feather
+                name='arrow-left' 
+                size={22} 
+                onPress={() => navigation.goBack()}
+                color='#f6f6f6' />
+            )
+          })}>
+            <Stack.Screen name='Welcome' component={Welcome} options={{ headerShown: false }}/>
+            
+          </Stack.Navigator>
+        </NoteProvider>
+      </NavigationContainer>
     )
   }
-  
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
